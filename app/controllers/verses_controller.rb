bc458bba7c20d32f638ebddf1aa4801e7cc5eca3
Brandon Carag <brandon.carag@gmail.com>
2014-08-28 14:30:17 -0700

    before turning off font awesome

--- app/controllers/verses_controller.rb
+++ app/controllers/verses_controller.rb
@@ -4,13 +4,13 @@ class VersesController < ApplicationController
 
 def show
   @verse=Verse.find(params[:id])
+  @category=@verse.categories.first
   passage=@verse.reference
   passage = passage.gsub(/\s/, "+")
   passage = passage.gsub(/\:/, "%3A")
   passage = passage.gsub(/\,/, "%2C")
   crossway_url="http://www.esvapi.org/v2/rest/passageQuery?key=IP&passage=#{passage}&include-headings=false&include-short-copyright=1"
   @verse_html=Net::HTTP.get(::URI.parse(crossway_url))
-  
 end
 
 end
